ffa4f2707711a7366b5c4d912c24594b6986783e
theint99 <theintshwesinmyomgy3199@gmailcom>
2022-04-05 14:49:17 +0630

    part one

---
+++ src/com/java/batch11/Lesson1.java
@@ -0,0 +1,51 @@
+package com.java.batch11;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I am Mi Theint");
+
+        //variable တစ်ခုဆောက်ချင်ရင်
+
+        //datatype
+        //variablename
+        //assignmentOperator
+        //value
+        //endOperator
+
+        /** documentation comment
+         * @author developer
+         *
+         *
+         *
+         *
+         */
+
+        String str="hello"; //initializaition (variable)
+
+        int number; //decleration, declare( value မထည့်ရသေး,variable)
+        number =99;
+
+        System.out.println(str);
+        System.out.println(number);
+
+        int num=15;
+        int num1=20;
+        System.out.println(num);
+
+
+        String name="Mg Mg";
+        System.out.println("I am"+name);
+        //System.out.println("I am"+name);
+
+        //Single Comment
+
+        /*Multiple
+
+        Comment*/
+
+        //Syntax
+        //namig convention ( class name ရှေ့စာလုံးအကြီးရေး/ variable ဆိုအသေးရေး)and naming rules (semicolumမပါတာတို့)
+    }
+}
+
